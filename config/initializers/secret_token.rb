defffcbfd6525d02273b97db8f030a088ea41dc0
Ali El-Ashram <ali.elashram@gmail.com>
2013-10-10 16:09:50 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MercyMarket::Application.config.secret_key_base = '65970e771e2fb52908ae7ec42955d099b050b452574045b439235ffac4799c377ac8cbbb6a3095e3d4a621f300d8cabfc3e2d704b84d1670b050882a227d3506'
